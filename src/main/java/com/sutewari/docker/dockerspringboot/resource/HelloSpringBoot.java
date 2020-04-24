89247a1c1109c2fa70ebc970e0a25cdbe7714436
e702276 <sutewari@gmail.com>
2020-04-23 23:35:12 -0500

    Docker Spring boot hello World - Auto Docker Build Test

--- src/main/java/com/sutewari/docker/dockerspringboot/resource/HelloSpringBoot.java
+++ src/main/java/com/sutewari/docker/dockerspringboot/resource/HelloSpringBoot.java
@@ -10,6 +10,6 @@ public class HelloSpringBoot {
 
     @GetMapping
     public String hello (){
-        return "Welcome to Spring boot Docker Hello World!";
+        return "Welcome to Spring boot Docker Hello World! Auto Docker Build Test";
     }
 }
